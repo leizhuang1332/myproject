714722eda8edeadab59e08d91ec7d4d2d3bae015
leizhuang <leizhuang@youzidata.com>
2019-11-03 14:40:21 +0800

    init

---
+++ src/main/java/youzidata/com/lz/controller/HelloController.java
@@ -0,0 +1,15 @@
+package youzidata.com.lz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("helloworld")
+    public String helloWorld(){
+        return "hello world";
+    }
+}
