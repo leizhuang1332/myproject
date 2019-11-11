3212fe3cd62466e74afadda4ca795e7aa8c207cb
leizhuang <leizhuang@youzidata.com>
2019-11-11 12:30:14 +0800

    mt2 add 桌面

--- src/main/java/youzidata/com/lz/gittest/MargeTest2.java
+++ src/main/java/youzidata/com/lz/gittest/MargeTest2.java
@@ -3,5 +3,6 @@ package youzidata.com.lz.gittest;
 public class MargeTest2 {
     public static void main(String[] args) {
         System.out.println("2");
+        System.out.println("idea");
     }
 }
